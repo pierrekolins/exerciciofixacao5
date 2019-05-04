a363707b77c03e9974d83d3b7dbc2daba006e2e6
pierrekolins <pierrepneto@gmail.com>
2019-05-04 12:39:25 -0300

    Criada a classe Conta

---
+++ ContaBancaria/Conta.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Conta
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta() {
+        }
+
+        public Conta(int numero, string titular) {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public void Deposito(double deposito){
+            Saldo += deposito;
+        }
+
+        public void Saque(double saque) {
+            Saldo -= (saque + 5);
+        }
+
+        public override string ToString()
+        {
+            return "Conta: "+Numero
+                +", Titular: "+Titular
+                +", Saldo: "+Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
